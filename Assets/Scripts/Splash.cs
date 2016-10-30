c81e82e8d688572c38ff8dc78d84797e08514991
Zalo <cebolleto@gmail.com>
2016-10-30 16:40:50 +0100

    splash

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splash : MonoBehaviour {
+
+	public void OnPlay() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("Game");
+	}
+}
