35ae605b24c18cfa994814a1ed66856a8d6475e2
Zalo <cebolleto@gmail.com>
2016-10-30 09:20:17 +0100

    added music

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour {
+
+	public AudioClip[] music;
+	public AudioSource source;
+	public float timing;
+	private int note;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Rhythm());
+	}
+
+	IEnumerator Rhythm () {
+		while (true) {
+			yield return new WaitForSeconds (timing);
+			source.PlayOneShot (music[note % music.Length]);
+			note++;
+		}
+	}
+}
