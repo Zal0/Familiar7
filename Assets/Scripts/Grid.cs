3ea36a3e255f64f9c7cfd0a3eb8d204b5aa2eb9a
Zalo <cebolleto@gmail.com>
2016-10-30 08:24:39 +0100

    Initial commit with first prototype

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	string[] grid = {
+		".--------",
+		".........",
+		".x.x.x.x.",
+		".........",
+		".x.x.x.x.",
+		".........",
+		".x.x.x.x.",
+		".........",
+		".x.x.x.x.",
+		"........."
+	};
+
+	public float cell_size = 4;
+	public GameObject tablePrefab;
+
+	void Start() {
+		int x_divs = grid [0].Length;
+		int y_divs = grid.Length;
+		for (int x = 0; x < x_divs; ++x) {
+			for (int y = 0; y < y_divs; ++y) {
+				if (GetTileAt (x, y) == 'x') {
+					GameObject.Instantiate (tablePrefab, GetPos(x, y), Quaternion.identity);
+				}
+			}
+		}
+	}
+
+	public char GetTileAt(int x, int y) {
+		if (x < 0 || x >= grid [0].Length || y < 0 || y >= grid.Length) {
+			return '-';
+		} else {
+			return grid[y][x];
+		}
+	}
+
+	public Vector3 GetPos(int x, int y) {
+		int x_divs = grid [0].Length;
+		int y_divs = grid.Length;
+		float width = (cell_size * x_divs);
+		float depth = (cell_size * y_divs);
+		float cell_w = width / x_divs;
+		float cell_h = depth / y_divs;
+
+		float start_x = transform.position.x - width / 2.0f;
+		float start_z = transform.position.z - depth / 2.0f;
+
+		return new Vector3 (start_x + x * cell_w + cell_w * 0.5f, 0.0f, start_z + y * cell_w + cell_w * 0.5f);
+	}
+
+	public void OnDrawGizmos() {
+		int x_divs = grid [0].Length;
+		int y_divs = grid.Length;
+		float width = (cell_size * x_divs);
+		float depth = (cell_size * y_divs);
+		float cell_w = width / x_divs;
+		float cell_h = depth / y_divs;
+
+		float start_x = transform.position.x - width / 2.0f;
+		float start_z = transform.position.z - depth / 2.0f;
+
+		for (int i = 0; i <= y_divs; ++i) {
+			Gizmos.DrawLine (new Vector3 (start_x, 0.0f, start_z + cell_h * i), new Vector3 (start_x + width, 0.0f, start_z + cell_h * i));
+		}
+		for (int i = 0; i <= x_divs; ++i) {
+			Gizmos.DrawLine (new Vector3 (start_x + cell_w * i, 0.0f, start_z), new Vector3 (start_x + cell_w * i, 0.0f, start_z + depth));
+		}
+
+		for (int x = 0; x < x_divs; ++x) {
+			for (int y = 0; y < y_divs; ++y) {
+				char tile = GetTileAt (x, y);
+				switch (tile) {
+					case '-':
+						Gizmos.color = Color.red;
+						break;
+
+					case '.':
+						Gizmos.color = Color.green;
+						break;
+
+					case 'x':
+						Gizmos.color = Color.yellow;
+						break;
+				}
+				Gizmos.DrawWireSphere (GetPos(x, y), 0.1f);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Grid grid;
+	public int grid_x, grid_y;
+	public Transform trayPivot;
+	public GameObject trayPrefab;
+	private Transform tray;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = grid.GetPos (grid_x, grid_y);
+
+		tray = (GameObject.Instantiate (trayPrefab) as GameObject).transform;
+		GetTray ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int progress_x = 0;
+		int progress_y = 0;
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			progress_y = 1;
+			transform.rotation = Quaternion.LookRotation (Vector3.forward);
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			progress_y = -1;
+			transform.rotation = Quaternion.LookRotation (Vector3.back);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			progress_x = -1;
+			transform.rotation = Quaternion.LookRotation (Vector3.left);
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			progress_x = 1;
+			transform.rotation = Quaternion.LookRotation (Vector3.right);
+		}
+
+		if ((progress_x != 0 || progress_y != -0)) {
+			char tile = grid.GetTileAt (grid_x + progress_x, grid_y + progress_y);
+			if (tile == '.') {
+				grid_x += progress_x;
+				grid_y += progress_y;
+				transform.position = grid.GetPos (grid_x, grid_y);
+				if (tray.parent != trayPivot) {
+					GetTray ();
+				}
+			} else if (tile == 'x') {
+				Vector3 pos = grid.GetPos (grid_x + progress_x, grid_y + progress_y) + Vector3.up * 0.4975f; 
+				ReleaseTray (pos);
+			}
+		}
+	}
+
+	void GetTray() {
+		tray.parent = trayPivot;
+		tray.localPosition = Vector3.zero;
+		tray.localRotation = Quaternion.identity;
+	}
+
+	void ReleaseTray(Vector3 pos) {
+		tray.parent = null;
+		tray.position = pos;
+	}
+}
