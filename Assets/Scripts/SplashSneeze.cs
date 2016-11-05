0f14bb75f8bd59483a01deba1f43d382452ba1ad
Zalo <cebolleto@gmail.com>
2016-11-05 18:17:32 +0100

    added gamejolt prefab and game objects in 3d

---
+++ Assets/Scripts/SplashSneeze.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashSneeze : MonoBehaviour {
+
+	public float timing = 1.0f;
+	public GameObject normal;
+	public GameObject sneeze;
+
+	void Start () {
+		StartCoroutine (Blink());
+	}
+
+	IEnumerator Blink () {
+		while (true) {
+			yield return new WaitForSeconds (timing);
+			normal.SetActive (false);
+			sneeze.SetActive (true);
+
+			yield return new WaitForSeconds (0.5f);
+			normal.SetActive (true);
+			sneeze.SetActive (false);
+		}
+	}
+}
